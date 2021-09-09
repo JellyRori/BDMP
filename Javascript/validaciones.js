e6602a7f9fd691cce7d3fc7e7343217f7120969e
Alex-Baz-Go99 <70982233+Alex-Baz-Go99@users.noreply.github.com>
2021-09-09 10:40:04 -0500

    camios4

--- Javascript/validaciones.js
+++ Javascript/validaciones.js
@@ -5,7 +5,10 @@ var formulario = document.getElementById('forma'),
     birthday = forma.birthday,
     rol = forma.rol,
     avatar = forma.avatar,
-    error = document.getElementById('error');
+    error = document.getElementById('error'),
+    regex =     /^(?=.*\d)(?=-*[!@#$%&*])(?=.*[a-z])(?=.*[A-Z]).{8,64}$/;
+    //regex =  "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})";
+
 
   function validarNombre(e){
     if (username.value == '' || username.value == null){
@@ -22,6 +25,13 @@ var formulario = document.getElementById('forma'),
       error.style.display = 'block';
       alert("Por favor completa la contraseña"); //escribiendo el error poniendolo en un alert
       e.preventDefault(); //detener la ejecucion (no envia el formulario)
+    }
+    else if(regex.test(password.value)==false)
+    {
+      error.style.display = 'block';
+      alert("La contraseña debe tener minimo 8 caracteres, un numero, una mayuscula y un character eespecial");
+      e.preventDefault(); //detener la ejecucion (no envia el formulario)
+    
     } else {
       error.style.display = 'none'; //resetear el error (en caso de no haber error)
     }
@@ -48,7 +58,63 @@ var formulario = document.getElementById('forma'),
     }
   }
 
- /* 
+function Contraseña(password){
+    
+    var MAYUS = false;
+    var minus = false;
+    var numero = false;
+    var caracter_especial = false;
+    
+    for(var i = 0;i<password.length;i++){
+        if(password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90){
+            MAYUS = true;
+        }
+        else{
+            
+        }
+        if(password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122)
+        {
+            minus = true;
+           
+        }
+        else{
+           
+        }
+        if(password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57)
+        {
+            numero = true;
+        }
+        else
+        {
+            caracter_especial = true;
+        }
+    }
+    if(MAYUS === true && minus === true && caracter_especial === true && numero === true){
+        password.style.display = 'none';
+       
+        return true;
+        
+    }
+    else{
+        password.style.display = 'block';
+        
+    }
+
+return false;
+}
+
+  /*  function test(e){
+    if(regex.test(document.getElementById("password").value)==false)
+    {
+      error.style.display = 'block';
+      alert("La contraseña debe tener minimo 8 caracteres, un numero, una mayuscula y un character eespecial");
+
+    }
+    else{
+      alert("usuario registrado");
+    }
+  }
+
 
   function validarFecha(e){
     if (birthday.HasValue== '' || birthday.HasValue == null){
@@ -94,6 +160,7 @@ var formulario = document.getElementById('forma'),
     validarPass(e);
     validarCorreo(e);
     validarElRol(e);
+   Contraseña(password);
     /*validarPass(e);
     validarCumple(e);
     validarAvatar(e);
