0fec914008533d4b3b502d2844418642b88130d7
JellyRori <maurasilva.cantu@hotmail.com>
2021-09-09 17:24:26 -0500

    ave

--- Javascript/validaciones.js
+++ Javascript/validaciones.js
@@ -20,7 +20,7 @@ var formulario = document.getElementById('forma'),
   }
 
   function validarPass(e){
-    var regex = regex =  /^(?=.*\d)(?=-*[!@#$%&*])(?=.*[a-z])(?=.*[A-Z]).{8,64}$/;
+    
     if (password.value == '' || password.value == null){
       error.style.display = 'block';
       alert("Por favor completa la contraseña"); //escribiendo el error poniendolo en un alert
@@ -28,6 +28,20 @@ var formulario = document.getElementById('forma'),
     }
     else {
       error.style.display = 'none'; //resetear el error (en caso de no haber error)
+    }
+  }
+
+function test(){
+  var regex = regex =  /^(?=.*\d)(?=-*[!@#$%&*])(?=.*[a-z])(?=.*[A-Z]).{8,64}$/;
+    if(regex.test(document.getElementById("password").value)==false)
+    {
+      error.style.display = 'block';
+      alert("La contraseña debe tener minimo 8 caracteres, un numero, una mayuscula y un character eespecial");
+
+    }
+    else{
+      return false;
+      alert("usuario registrado");
     }
   }
 
@@ -56,54 +70,6 @@ var formulario = document.getElementById('forma'),
 
 
 
-  /*  function test(e){
-    if(regex.test(document.getElementById("password").value)==false)
-    {
-      error.style.display = 'block';
-      alert("La contraseña debe tener minimo 8 caracteres, un numero, una mayuscula y un character eespecial");
-
-    }
-    else{
-      alert("usuario registrado");
-    }
-  }
-
-
-  function validarFecha(e){
-    if (birthday.HasValue== '' || birthday.HasValue == null){
-      error.style.display = 'block';
-      alert("Por favor ponga su fecha de nacimiento (y no olvide la hora)");
-      e.preventDefault();
-    } else {
-      error.style.display = 'none';
-    }
-  }
-
-  function validarAvatar(e){
-    if (avatar.value == '' || avatar.value == null){
-      error.style.display = 'block';
-      alert("Por favor elija su preferencia");
-      e.preventDefault();
-    } else {
-      error.style.display = 'none';
-    }
-  }
-
-/´posible ayuda para lo especial de la contraseña----------------------------------------------
-  function validarCorreo(correo){
-   var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/; 
-   var esValido= expReg.test(correo);
-
-   if(esValido==true){
-    alert('El correo electronico es valido');
-
-   }else{
-    alert('El correo electronico NO es valido');
-   }
-}
-
-
-*/
   //Funcion global para ejecutar todas las funciones de validacion------------------------------------
   function validarForm(e){
     error.innerHTML = ''; //que el error esté en blanco (para que no se acumulen los mensajes al picar el boton)
